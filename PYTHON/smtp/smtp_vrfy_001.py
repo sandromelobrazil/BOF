0d8b192f0f69a8d43e0d85579d48463cd9b063bb
sandromelobrazil <sandromelo.brazil@gmail.com>
2019-07-28 03:01:01 +0100

    28/07/2019-03:00:01 - First Commit

--- PYTHON/smtp/smtp_vrfy_001.py
+++ PYTHON/smtp/smtp_vrfy_001.py
@@ -6,15 +6,25 @@ remoteip = "10.171.1.129"
 port=int("25") 
 user='msfadmin' 
 command='VRFY' 
+msg='helo server' 
 
 print("\nTeste de Conexao com o servidor SMTP... " + remoteip )
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((remoteip,port))
+
+greeting=encode('helo mailserver'}
+s.send(greeting)
 data = s.recv(1024)
 print(data)
 
-s.send('VRFY'+'\r\n')
+msg=(command + ' ' + user + ' ' + '\r\n' )
+
+sendmsg=msg.encode()
+
+print(sendmsg)
+
+s.send(sendmsg)
 data = s.recv(1024)
 print(data)
 
