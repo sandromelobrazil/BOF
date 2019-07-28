d2e7b2325d452f1560179753dc953e10b317a1b0
sandromelobrazil <sandromelo.brazil@gmail.com>
2019-07-28 04:00:57 +0100

    28/07/2019-04:00:01 - First Commit

--- PYTHON/smtp/smtp_vrfy_003.py
+++ PYTHON/smtp/smtp_vrfy_003.py
@@ -5,19 +5,21 @@ remoteip = "10.171.1.129"
 port=int("25") 
 command='VRFY' 
 
-user1='msfadmin' 
 #print("Teste de Conexao com o servidor SMTP... " + remoteip )
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((remoteip,port))
 data = s.recv(1024)
 print(data)
 
+
 ### running commad
-smtpcmd=(command + ' ' + user1 + ' ' + ' \r\n')
-sendsmtpcmd=smtpcmd.encode()
-s.send(sendsmtpcmd)
-data = s.recv(1024)
-print(data)
+with open('userlist.txt') as _dictionary:
+    for _username in _dictionary:
+        smtpcmd=(command + ' ' + _username + ' ' + ' \n')
+        sendsmtpcmd=smtpcmd.encode()
+        s.send(sendsmtpcmd)
+        data = s.recv(1024)
+        print(data)
 
 s.close()
-print("\n Conta " + user1 +  " verificada!")
+#print("\n Conta verificada!")
